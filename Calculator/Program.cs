26fe36736c0938f22d85baffc719351c92b5234f
c-H097 <meriemh706@gmail.com>
2026-09-24 20:42:14 +0300

    implement program code

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("new line");
+﻿
+// int variable stores the first number 
+Console.WriteLine("Enter the first number followed by the Enter key: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine()); //Convert the string input to a 32 bit integer value
+
+
+// int variable stores the second number 
+Console.WriteLine("Enter the second number followed by the Enter key: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+//perform calculation
+int result = firstNumber + secondNumber;
+
+//output answer to console
+Console.WriteLine("The result of adding {0} and {1}  is {2}", firstNumber, secondNumber, result); //{o} is a placeholder for the result variable
+
+
